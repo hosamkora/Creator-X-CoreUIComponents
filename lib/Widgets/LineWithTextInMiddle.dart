155419b5796d7aacf4e779c8204f5c1daf0bac9d
hosam <hosamkora@gmail.com>
2019-09-19 16:48:18 +0200

    refactor some code and add line thichness option

--- lib/Widgets/LineWithTextInMiddle.dart
+++ lib/Widgets/LineWithTextInMiddle.dart
@@ -4,36 +4,42 @@ import 'package:flutter/material.dart';
 class LineWithMiddleText extends StatelessWidget {
   final String text;
   final Color color;
+  final double textHorizontalPaading;
+  final double lineThickness;
 
-  const LineWithMiddleText({Key key, @required this.text, this.color})
-      : super(key: key);
+  const LineWithMiddleText({
+    Key key,
+    @required this.text,
+    this.color = Colors.yellow,
+    this.textHorizontalPaading = 15,
+    this.lineThickness = 1.0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _color = color != null ? color : Colors.yellow;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         Flexible(
           child: Divider(
-            color: _color,
-            thickness: 1.0,
+            color: color,
+            thickness: lineThickness,
           ),
           flex: 2,
         ),
         Center(
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: EdgeInsets.symmetric(horizontal: textHorizontalPaading),
             child: TitleText(
               text,
-              color: _color,
+              color: color,
             ),
           ),
         ),
         Flexible(
           child: Divider(
-            color: _color,
-            thickness: 1,
+            color: color,
+            thickness: lineThickness,
           ),
           flex: 2,
         ),
