5c5f5a096c5c032ad14806c5c9ad32c10612d84d
hosamkora <hosamkora@gmail.com>
2019-09-24 11:30:16 +0200

    Revert "Page header"

--- lib/Utilities/Constants.dart
+++
@@ -1,9 +0,0 @@
-class Constants {
-  /// supported platforms codes
-  static const IOS = 'ios';
-  static const ANDROID = 'ios';
-
-  /// supported languages codes
-  static const EN = 'en';
-  static const AR = 'ar';
-}
